3097a5514cc8acca390106eb0a76feb963e3ff66
JuleeKeenanRivers <swimgirljakr@gmail.com>
2016-09-27 10:23:35 -0700

    Added lesson 01 and 02

---
+++ PYLesson_02/programlesson_02.py
@@ -0,0 +1,7 @@
+name="ProfessorHandsome"
+print (name) 
+first="professor"
+second= "handsome"
+print(first+second) 
+kids=38
+print("there are",kids, "kids in our class.") 
--- PyLesson_01/Lab_01.py
+++ PyLesson_01/Lab_01.py
@@ -1,3 +1,6 @@
 print("hello world")
 print("hello again") 
 print("   \"\"\  ""   ")
+print("(>__<)") 
+print("@..@")
+
