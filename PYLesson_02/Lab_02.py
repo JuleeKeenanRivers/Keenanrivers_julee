5864dbdae12ef4385f913ea2c14a7e83b41a2747
JuleeKeenanRivers <swimgirljakr@gmail.com>
2016-10-03 10:12:50 -0700

    Completed Lab_03

---
+++ PYLesson_02/Lab_02.py
@@ -0,0 +1,12 @@
+num1 = 45
+num2 = 12
+print(num1 * num2) 
+name = "Julee Keenan-Rivers"
+address = "1159 Wales Place"
+zipcode = "cardiff, CA 92007"
+print(name+ "\n" +address+ "\n" +zipcode)
+l = 4
+w = 5
+h = 7
+recsurfaceArea = 2 * w * l + 2 * l * h + 2 * h * w
+print("The area of your rectangle is", recsurfaceArea)
