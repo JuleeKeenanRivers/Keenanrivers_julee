b545d10aab70cf7239159d6d5be85214bf13fc15
JuleeKeenanRivers <swimgirljakr@gmail.com>
2016-10-04 12:45:14 -0700

    This is my lab_03 Ex_01.

---
+++ PYLesson_03/Lab_03Ex_01.py
@@ -0,0 +1,28 @@
+print ("Hi, my name is RudeAI. I'd like to ask you a few questions.")
+
+name = input( "What is your name? " )
+
+
+print(name, "!!" )
+
+print (name + " Why would anyone name a baby that?")
+
+age = input( "How old are you, " + name + " ? " )
+
+print( "Ooo!!!"+ age + " is getting up there." )
+
+fun = input( " What do you do for fun, " + name + " ? " ) 
+ 
+print( " I thought only nerds like to" + fun ) 
+
+music = input ( " What kind of music do you like? ")
+
+print( " Booo! I wouldnt wish the sound of" + music + " on my worst enemy. ")
+
+siblings = input( " How many siblings do you have? ")
+
+print( siblings + " ? Wow, I hope the rest of your family is better looking. " )
+
+job = input( " What do you want to be when you grow up? " )
+
+print( " I think youd have to be smarte to be " + job + " So " + name + " youre " + age + " you like to " + fun + " and listen to " + music + " Good luck becoming a " + job + "Im only kiddin' " + name ) 
