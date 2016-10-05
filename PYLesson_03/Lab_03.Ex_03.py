9c134f7534a4b3cf3184b1467550310c2d689009
JuleeKeenanRivers <swimgirljakr@gmail.com>
2016-10-05 10:25:15 -0700

    This is my Lab_03Ex_02 and my Lab_03Ex_03

---
+++ PYLesson_03/Lab_03.Ex_03.py
@@ -0,0 +1,6 @@
+num = int(input ( " how many cats do you have: " ))
+          
+num1 = int(input ( "how many dogs do you have: " ))
+
+print ( " You have " , num+num1 , " pets " )
+           
---
+++ PYLesson_03/Lab_03Ex_02.py
@@ -0,0 +1,5 @@
+num1 = int (input( " please enter your height: " ))
+num2 = int (input( " please enter your weight: " ))
+
+print("Your BMI is: ", (num2/(num1*num1))*703)
+
