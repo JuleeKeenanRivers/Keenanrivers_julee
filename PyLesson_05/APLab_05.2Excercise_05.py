cf6b6dd3799d5950cf5be9e84e2532f1149f7a37
JuleeKeenanRivers <swimgirljakr@gmail.com>
2016-11-08 10:22:02 -0800

    changes to lab_05

---
+++ PyLesson_05/APLab_05.2Excercise_05.py
@@ -0,0 +1,32 @@
+print("One day you were walking down the street and you saw a monster")
+choice = input("Do you kill the monster or continue on with you walk (k or w)") 
+if choice == "k":
+    print ("You attacked the  monster and survived")
+if choice == "w":
+    print("You died")
+print("You continue down the street and you find a thousand dollars")
+choice = input("Do you take the money or continue walking(t or w)")
+if choice == "t":
+     print("You were arrested and died")
+if  choice == "w":
+     print("You continuing walking down the street")
+else:
+     #end of program
+else:
+    if choice == c:
+            #end of program
+        else:
+            #end of program
+
+else: #if choice == "w";
+    if choice == a:
+        if choice == b:
+            #end of program
+        else:
+            #end of program
+    else:
+        if choice == c:
+            #end of program
+        else:
+            #end of program
+            
