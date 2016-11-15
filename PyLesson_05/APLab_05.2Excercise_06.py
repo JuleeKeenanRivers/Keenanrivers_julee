2c01637c16b926d7a87d93cc3a3d8e9935791bf4
JuleeKeenanRivers <swimgirljakr@gmail.com>
2016-11-15 09:33:52 -0800

    new changes

--- PyLesson_05/APLab_05.2Excercise_06.py
+++ PyLesson_05/APLab_05.2Excercise_06.py
@@ -1,4 +1,16 @@
-username = juleekr
-password = password
+user = "juleekr"
+passw = "password"
+username = input("Enter your username here:")
+password = input("Enter your password here:")
 
- 
+def passCheck():
+    if username == "juleekr" and password == "password":
+        print("Access granted!")
+    elif username == "juleekr" and password != "password":
+        print("Wrong password")
+    elif username != "juleekr" and password == "password":
+        print("Wrong password")
+    else:
+        print("wrong password and username")
+passCheck() 
+
