15ce103e37c803f299a45cd0a3e753ad1b6e7ca6
JuleeKeenanRivers <swimgirljakr@gmail.com>
2017-01-05 10:24:16 -0800

    new changes to lab_09

--- PyLesson_09/PyLab_09Ex03.py
+++ PyLesson_09/PyLab_09Ex03.py
@@ -1,10 +1,31 @@
-newIntegerList = ["1, 4, 7, 10, 12, 26"]
+import random
+numbers = []
 
-def average(newIntegerList):
-    for i in range(0,10):
-        print(newIntegerList[i])
+for i in range(0,10):
+    numbers.append(random.randint(0,100))
 
-print(newIntegerList)
+output = ""
+for value in numbers:
+    output += str(value) + " "
+
+print(output)
+
+print("")
+
+def average(LiSt):
+    summ = 0
+    for num in LiSt:
+        summ += num
+    return(summ/len(LiSt))
+
+
+print("The average of the above numbers is..." + str(average(numbers)))
+
+
+
+        
+        
+
 
 
 
--- PyLesson_09/PyLab_09Ex04.py
+++ PyLesson_09/PyLab_09Ex04.py
@@ -1,3 +1,12 @@
 startnumber = int(input("Please enter a starting number:"))
 sequencesize = int(input("Please enter a sequence size:"))
 
+seq = len(str(sequencesize))
+
+for i in range(0,seq):
+    if i is 0 or 1:
+        seq[i] = 0
+    else:
+        seq[i] = seq%20
+print(seq)
+
