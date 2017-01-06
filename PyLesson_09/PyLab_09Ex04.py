4bbce6b5f12ae1400941e8c8ea103dc757a3131d
JuleeKeenanRivers <swimgirljakr@gmail.com>
2017-01-06 10:24:39 -0800

    new changes to lab_09

--- PyLesson_09/PyLab_09Ex04.py
+++ PyLesson_09/PyLab_09Ex04.py
@@ -1,12 +1,13 @@
 startnumber = int(input("Please enter a starting number:"))
 sequencesize = int(input("Please enter a sequence size:"))
 
-seq = len(str(sequencesize))
+seq = [0] * sequencesize
 
-for i in range(0,seq):
-    if i is 0 or 1:
-        seq[i] = 0
+for i in range(0,len(seq)):
+    if i == 0 or i == 1:
+        seq[i] = startnumber
     else:
-        seq[i] = seq%20
+        seq[i] = seq[i-2] + seq[i-1]
 print(seq)
+
 
---
+++ PyLesson_09/PyLab_09Ex06.py
@@ -0,0 +1,15 @@
+expression = int(input("Please enter a mathematical expression:"))
+
+equation = expression.split()
+
+while i < len(equation) and (equation[i] = "*" or "/"):
+    if equation[i] = "*":
+        equation[i] = int(equation(i-1)*int(i+1)
+    else:
+        equation[i] = int(equation[i-1]/=int(i+1)
+    
+    equation.remove[i-1]
+    equation.remove[i]
+1+i
+
+print(equation) 
