7b88a19288c50dca33cd9f3efb06cdfa3dbbbded
JuleeKeenanRivers <swimgirljakr@gmail.com>
2017-01-09 10:25:18 -0800

    new changes to lesson_09

--- PyLesson_09/PyLab_09Ex06.py
+++ PyLesson_09/PyLab_09Ex06.py
@@ -2,14 +2,36 @@ expression = int(input("Please enter a mathematical expression:"))
 
 equation = expression.split()
 
-while i < len(equation) and (equation[i] = "*" or "/"):
-    if equation[i] = "*":
-        equation[i] = int(equation(i-1)*int(i+1)
-    else:
-        equation[i] = int(equation[i-1]/=int(i+1)
-    
-    equation.remove[i-1]
-    equation.remove[i]
-1+i
+while i < len(equation):
+    if i < len(equation) and (equation[i] == "*" or "/"):
+        if equation[i] == "*":
+            equation[i] = int(equation(i-1)*int(i+1)
+        equation[i] = int(equation[i-1] % int(i+1)
+        nums.remove(equation[i-1])
+        nums.remove(equation[i])
+    print(1+i)
 
-print(equation) 
+
+i = 0
+
+while i < len(equation):
+    if i < len(equation) and (equation[i] == "+" or "-"):
+        if equation[i] == "+":
+            equation[i] = int(equation[i-1])+int(equation[i+1])
+        else:
+            equation[i] = (int(equation[i-1])-int(equation[i+1]))
+        nums.remove(equation[i-1])
+        nums.remove(equation[i])
+    print(1+i)
+print(equation)
+
+                              
+            
+            
+                            
+                              
+            
+                              
+
+ 
+
