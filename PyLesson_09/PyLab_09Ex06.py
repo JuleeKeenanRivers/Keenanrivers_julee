f9263670ca7cedc99fbb91d4628c2049840534e1
JuleeKeenanRivers <swimgirljakr@gmail.com>
2017-01-10 10:24:57 -0800

    new changes to lesson_09 n

--- PyLesson_09/PyLab_09Ex06.py
+++ PyLesson_09/PyLab_09Ex06.py
@@ -1,34 +1,40 @@
-expression = int(input("Please enter a mathematical expression:"))
+expression =input("Please enter a mathematical expression:")
 
-equation = expression.split()
+equation = expression.split(" ")
+print (equation)
 
+i=0
 while i < len(equation):
-    if i < len(equation) and (equation[i] == "*" or "/"):
+    if i < len(equation) and (equation[i] == "*" or equation[i] == "/"):
         if equation[i] == "*":
-            equation[i] = int(equation(i-1)*int(i+1)
-        equation[i] = int(equation[i-1] % int(i+1)
-        nums.remove(equation[i-1])
-        nums.remove(equation[i])
-    print(1+i)
-
+            equation[i] = int(equation[i-1]) * int(equation[i+1])
+        else:
+            equation[i] = int(equation[i-1]) / int(equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    i+=1
 
 i = 0
-
 while i < len(equation):
-    if i < len(equation) and (equation[i] == "+" or "-"):
+    if i < len(equation) and (equation[i] == "+" or equation[i] == "-"):
         if equation[i] == "+":
-            equation[i] = int(equation[i-1])+int(equation[i+1])
+            equation[i] = int(equation[i-1]) + int(equation[i+1])
         else:
-            equation[i] = (int(equation[i-1])-int(equation[i+1]))
-        nums.remove(equation[i-1])
-        nums.remove(equation[i])
-    print(1+i)
+            equation[i] = int(equation[i-1]) - int(equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    i+=1
 print(equation)
+                          
+
+
+
 
                               
             
             
                             
+
                               
             
                               
