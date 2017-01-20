6b05d0a5d74d319c15d5dc4950b4b6ff423fdac6
JuleeKeenanRivers <swimgirljakr@gmail.com>
2017-01-20 10:24:38 -0800

    new changes to 10 and 11

--- PyLesson_10/PyLab_10Ex_03.py
+++ PyLesson_10/PyLab_10Ex_03.py
@@ -1,17 +1,18 @@
+import random 
 xAndO = []
 for i in range(0,4):
     xAndO.append([])
     for j in range(0,4):
-        switch = xAndO.append(random.randint(0,1))
+        switch = random.randint(0,1)
         if switch == 1:
-            xAndO[i] += "x"
-        xAndO[i] += "O"
-for values in XandO:
-    output = " "
-    for value in values:
-        output += value
-    print(output) 
-
+            xAndO[i].append("x")
+        else:
+            xAndO[i].append("o")
         
-    
+for values in xAndO:
+    output = ""
+    for value in values:
+        output += value + "\t"
+    print(output) 
+  
 
