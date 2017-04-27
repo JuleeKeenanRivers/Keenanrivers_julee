d958b583fe3c7cdfd92e0220d12f0b3e0a4f8419
JuleeKeenanRivers <swimgirljakr@gmail.com>
2017-04-27 10:23:16 -0700

    new changes

--- 1st Website/Weblab_14/weblab_14global/script.js
+++ 1st Website/Weblab_14/weblab_14global/script.js
@@ -1,43 +1,45 @@
 /**
  * Created by keenanriversj1816 on 4/26/2017.
  */
+// function validate(){
+//     var x = document.forms.input.username.value;
+//     var atPos = x.indexOf("@");
+//     var dotPos = x.lastIndexOf(".");
+//
+//
+//     if(atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length)
+//         alert("This is not a valid email address!");
+//
+//     else
+//         alert("Success!");
+// }
+//
+// function validate(y){
+//     if( y < 6) {
+//         alert("This is not a valid password!");
+//         return false;
+//     }
+//     else
+//         return true;
+// }
+
 function validate(){
     var x = document.forms.input.username.value;
+    var y = document.forms.input.password.value;
+
     var atPos = x.indexOf("@");
     var dotPos = x.lastIndexOf(".");
 
-
-    if(atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length)
-        alert("This is not a valid email address!");
-
-    else
-        alert("Success!");
-}
-
-function validate(){
-    var y = document.forms.input.password.value;
-
-    if( y < 6)
-        alert("This is not a valid password!");
-
-    else
-        alert("Success!");
-}
-
-function validate(){
-    var x = document.forms.input.username.value;
-    var atPos = x.indexOf("@");
-    var dotPos = x.lastIndexOf(".");
-    var y = document.forms.input.password.value;
-    
-    if(atPos , 1 || dotPos < atPos + 2 || dotPos + 2 > x.length || y < 6)
+    //if username and password are not correct
+    if((atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length) && y.length < 6)
         alert("This is not a valid email address or password!");
-    
-    else
-        alert("Success!");
-        
-    
-    
-    
+    else {
+        if (atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length)
+            alert("Username is not correct.");
+        else if (y < 6)
+            alert("Password is not correct.");
+        else
+            alert("Success!");
+    }
 }
     
